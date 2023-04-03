3fd0ed98bcab48baf205c4754fc2ff89aac067be
IsaacMayard <isaacmayard@gmail.com>
2023-04-03 09:49:02 -0600

    Completed level 2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,51 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:8080/titles")
       .then((res) => res.json())
-      .then((titles) => setData(titles));
+      .then((titles) => setData(titles))
+      .catch((err) => console.log("ERROR:", err));
   }, []);
+
+  const handleClick = () => {
+    let matches = data.filter(
+      (movie) => movie.title.toLowerCase() === searchTerm.toLowerCase()
+    );
+    if (matches.length === 0) {
+      setSearchData(["Sorry, no matches found"]);
+    } else {
+      setSearchData(matches);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {data.map((movie, index) => {
-          return <p key={index}>Title: {movie.title}</p>;
-        })}
+        {searchData.length
+          ? searchData.map((movie, index) =>
+              movie.title ? (
+                <p key={index}>Title: {movie.title}</p>
+              ) : (
+                <p>{searchData}</p>
+              )
+            )
+          : data.map((movie, index) => {
+              return <p key={index}>Title: {movie.title}</p>;
+            })}
+        <input
+          type="text"
+          placeholder={"Search a Movie"}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="button" onClick={handleClick}>
+          Submit
+        </button>
       </header>
     </div>
   );
